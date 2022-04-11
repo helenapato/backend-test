c6b07fb6877d78fbd96f6fb0db1a45fa309e412f
Helena Pato <lenapato.m@gmail.com>
2022-04-11 11:04:49 -0300

    Atualizando migration

--- priv/repo/migrations/20220222184925_create_users.exs
+++ priv/repo/migrations/20220222184925_create_users.exs
@@ -2,8 +2,6 @@ defmodule ApiBlogs.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    #drop table("posts")
-    #drop table("users")
     create table(:users) do
       add :displayName, :string
       add :email, :string
